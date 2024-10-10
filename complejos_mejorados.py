4f4805af835cfe434bd128b306930841bc365957
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-09 20:09:37 -0600

    Update complejos_mejorados.py

--- complejos_mejorados.py
+++ complejos_mejorados.py
@@ -9,7 +9,15 @@ import math
 import matplotlib.pyplot as plt
 from colorama import Fore, Style
 
-import cmath
-import math
-import matplotlib.pyplot as plt
-from colorama import Fore, Style
+# Función para mostrar el menú de opciones
+def menu():
+    print("----------MENU----------")
+    print(f"{Fore.GREEN}1. SUMA")
+    print(f"{Fore.YELLOW}2. RESTA")
+    print(f"{Fore.BLUE}3. MULTIPLICACION")
+    print(f"{Fore.MAGENTA}4. DIVISION")
+    print(f"{Fore.CYAN}5. POTENCIAS A LA N")
+    print(f"{Fore.RED}6. POTENCIAS DE i")
+    print(f"{Fore.GREEN}7. RAICES DE NUMEROS COMPLEJOS")
+    print(f"{Fore.YELLOW}8. SALIR")
+    print(Style.RESET_ALL)
