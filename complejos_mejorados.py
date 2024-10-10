efee0512a779c61fb8c2d2a6524635d5eb51af69
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-10-09 20:09:01 -0600

    Update complejos_mejorados.py

--- complejos_mejorados.py
+++ complejos_mejorados.py
@@ -2,3 +2,9 @@
 # Fecha: 09 octubre 2024
 # Descripción: Código para complejos mejorados.
 # GitHub: https://github.com/Jona163
+
+
+import cmath
+import math
+import matplotlib.pyplot as plt
+from colorama import Fore, Style
